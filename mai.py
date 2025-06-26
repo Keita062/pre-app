89bc163d8209609dddd0fa0b7c803ae7d23e126a
鈴木啓太 <22311104ks@tama.ac.jp>
2025-06-26 12:44:19 +0900

    try(06.26)

---
+++ mai.py
@@ -0,0 +1,5 @@
+# https://gemini.google.com/app/0267c621c056beae?utm_source=app_launcher&utm_medium=owned&utm_campaign=base_all
+import pandas as pd
+import numpy as np
+df = pd.read_csv(r"C:\Users\sk062\Downloads\df_intern_27.csv")
+df
