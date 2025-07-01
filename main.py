8668a37509482b01d2254bf84b34725b069aa7b3
鈴木啓太 <22311104ks@tama.ac.jp>
2025-07-02 00:54:44 +0900

    Completed pre-app(07.02)

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ height_50dp = "50dp"
 
 # 表示テキストの定義
 next_text = "next"
+reset_text = "Return to top"
 
 # メインの画面となるレイアウトを定義するクラスです。
 # BoxLayoutを継承しており、ウィジェット（部品）を縦か横に並べることができます。
@@ -59,6 +60,15 @@ class MainScrean( BoxLayout ):
 
         self.next_button.bind(on_press=self.go_to_next_screen)
 
+        self.reset_button = Button(
+            text =reset_text,
+            font_size = font_size_2nd,
+            size_hint_y = None ,
+            height = height_50dp
+        )
+        
+        self.reset_button.bind(on_press=self.reset_to_first_screen)
+
         self.add_widget(self.main_label)
         self.add_widget(self.next_button)
     
@@ -70,6 +80,13 @@ class MainScrean( BoxLayout ):
 
             if self.screen_index == len(self.screen_text) - 1:
                 self.remove_widget(self.next_button)
+                self.add_widget(self.reset_button)
+
+    def reset_to_first_screen(self, instance):
+        self.screen_index = 0
+        self.main_label.text = self.screen_text[self.screen_index]
+        self.remove_widget(self.reset_button)
+        self.add_widget(self.next_button)
 
 class ScreenTransitionApp(App):
 
